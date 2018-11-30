ef231115f2afa05eb770e8006886baf4ce7443b9
aapolimeno <45237388+aapolimeno@users.noreply.github.com>
2018-11-30 23:16:09 +0100

    een nieuwe versie die ook vrij weinig doet
    - heeft wel supposedly kloppende wiskunde erin 
    - ik ben te moe om uit te zoeken wat er precies mis is dus kijk hier later wel naar 
    - groetjes

---
+++ practicum2/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Practicum2
+{
+    partial class Mandelbrot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.MiddenX = new System.Windows.Forms.TextBox();
+            this.MiddenY = new System.Windows.Forms.TextBox();
+            this.MaxWaarde = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SchaalWaarde = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "midden X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "midden Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 51);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "schaal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 90);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "max";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(786, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 86);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MiddenX
+            // 
+            this.MiddenX.Location = new System.Drawing.Point(89, 12);
+            this.MiddenX.Margin = new System.Windows.Forms.Padding(4);
+            this.MiddenX.Name = "MiddenX";
+            this.MiddenX.Size = new System.Drawing.Size(132, 22);
+            this.MiddenX.TabIndex = 5;
+            this.MiddenX.TextChanged += new System.EventHandler(this.MiddenX_TextChanged);
+            // 
+            // MiddenY
+            // 
+            this.MiddenY.Location = new System.Drawing.Point(89, 50);
+            this.MiddenY.Margin = new System.Windows.Forms.Padding(4);
+            this.MiddenY.Name = "MiddenY";
+            this.MiddenY.Size = new System.Drawing.Size(132, 22);
+            this.MiddenY.TabIndex = 6;
+            this.MiddenY.TextChanged += new System.EventHandler(this.MiddenY_TextChanged);
+            // 
+            // MaxWaarde
+            // 
+            this.MaxWaarde.Location = new System.Drawing.Point(89, 87);
+            this.MaxWaarde.Margin = new System.Windows.Forms.Padding(4);
+            this.MaxWaarde.Name = "MaxWaarde";
+            this.MaxWaarde.Size = new System.Drawing.Size(132, 22);
+            this.MaxWaarde.TabIndex = 8;
+            this.MaxWaarde.TextChanged += new System.EventHandler(this.MaxWaarde_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 15);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(110, 21);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(130, 17);
+            this.radioButton6.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(110, 21);
+            this.radioButton6.TabIndex = 12;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "radioButton6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.SchaalWaarde);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.MaxWaarde);
+            this.groupBox1.Controls.Add(this.MiddenY);
+            this.groupBox1.Controls.Add(this.MiddenX);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(37, 16);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(476, 116);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // SchaalWaarde
+            // 
+            this.SchaalWaarde.Location = new System.Drawing.Point(289, 49);
+            this.SchaalWaarde.Margin = new System.Windows.Forms.Padding(4);
+            this.SchaalWaarde.Name = "SchaalWaarde";
+            this.SchaalWaarde.Size = new System.Drawing.Size(132, 22);
+            this.SchaalWaarde.TabIndex = 10;
+            this.SchaalWaarde.TextChanged += new System.EventHandler(this.SchaalWaarde_TextChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(289, 12);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(139, 56);
+            this.trackBar1.TabIndex = 9;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(521, 15);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(257, 117);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(130, 87);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(110, 21);
+            this.radioButton4.TabIndex = 16;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(12, 87);
+            this.radioButton5.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(110, 21);
+            this.radioButton5.TabIndex = 15;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "radioButton5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(130, 51);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(110, 21);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 51);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(110, 21);
+            this.radioButton3.TabIndex = 13;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(37, 139);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(849, 585);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Mandelbrot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(912, 736);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Mandelbrot";
+            this.Text = "Mandelbrot";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox MiddenX;
+        private System.Windows.Forms.TextBox MiddenY;
+        private System.Windows.Forms.TextBox MaxWaarde;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox SchaalWaarde;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ practicum2/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Globalization;
+
+/*Comment section
+    -   De event handlers voor de tekstboxes hebben nu als trigger TextChanged,
+        maar is het niet handiger om ze te triggeren als je op de OK knop klikt?
+    -   
+*/
+namespace Practicum2
+{
+    public partial class Mandelbrot : Form
+    {
+
+        public Mandelbrot()
+        {
+            InitializeComponent();
+        }
+
+        void ShowMandelbrot(object o, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            for (int x = 0; x < pictureBox1.Width; x++)
+            {
+                for (int y = 0; y < pictureBox1.Height; y++)
+                {
+                    double a = (double)(x - (pictureBox1.Width / 2)) / (double)(pictureBox1.Width / 4);
+                    double b = (double)(y - (pictureBox1.Width / 2)) / (double)(pictureBox1.Width / 4);
+                    Complex c = new Complex(a, b);
+                    Complex z = new Complex(0, 0);
+                    // hier wordt de iteratie geregeld. de iteratie stopt als de magnitude groter is dan 2, of als hij 100 keer heeft geitereerd. 
+                    // als een punt meer dan 100 iteraties heeft, is hij buiten de Mandelbrot set
+                    // als een punt minder dan 100 iteraties heeft, is hij binnen de Mandelbrot set
+                    int it = 0;
+                    do
+                    {
+                        it++;
+                        z.Square();
+                        z.Add(c);
+                        if (z.Magnitude() > 2.0) break;
+                    }
+                    while (it < 100);
+                    bm.SetPixel(x, y, it < 100 ? Color.Black : Color.White);
+                }
+
+
+                pictureBox1.Image = bm;
+
+            }
+        }
+        void MiddenX_TextChanged(object sender, EventArgs ea)
+        {
+
+
+            this.Invalidate();
+        }
+
+        void MiddenY_TextChanged(object sender, EventArgs e)
+        {
+
+
+            this.Invalidate();
+
+        }
+
+        void MaxWaarde_TextChanged(object sender, EventArgs e)
+        {
+            //functie die maximum mandelgetal instelt
+
+            this.Invalidate();
+
+        }
+
+        void SchaalWaarde_TextChanged(object sender, EventArgs e)
+        {
+            //functie die zoom instelt
+            //evt ook functie die laat zoomen met scroll
+
+            this.Invalidate();
+
+        }
+    }
+
+    public class Complex
+    {
+        public double a; //real part
+        public double b; //imaginary part, includes the i
+
+        // constructor voor de complex numbers
+        public Complex(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void Square()
+        {
+            double temp = (a * a) - (b * b);
+            b = 2.0 * a * b;
+            a = temp;
+        }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt((a * a) + (b * b));
+        }
+
+        public void Add(Complex c)
+        {
+            a += c.a;
+            b += c.b;
+        }
+    }
+
+}
+
+
+        
+   
